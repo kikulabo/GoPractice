6651ff1d7bda390e37052d70f14f77fda4a0d414
kikulabo <kikuchi@x-tech5.co.jp>
2025-09-29 20:28:37 +0900

    feat: exercise-maps.go を追加

---
+++ TourOfGo/exercise-maps/exercise-maps.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	counts := make(map[string]int)
+
+	for _, word := range words {
+		counts[word]++
+	}
+
+	return counts
+}
+
+func main() {
+	wc.Test(WordCount)
+}
